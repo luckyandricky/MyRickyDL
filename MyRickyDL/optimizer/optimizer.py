a1f9c2b1a6a695e3fdf838e826d59a953961a256
unknown <38056478+luckyandricky@users.noreply.github.com>
2021-11-23 20:51:59 +0800

    optimizer

--- MyRickyDL/optimizer/optimizer.py
+++ MyRickyDL/optimizer/optimizer.py
@@ -1,3 +1,5 @@
+import abc
+
 import numpy as  np
 from ..kernel import Node, Variable, get_node_from_graph
 from ..kernel.grapth import CalculateGrapth
@@ -17,6 +19,57 @@ class Optimizer(object):
     def one_step(self):
 
         """add Gradient of samples"""
-        self.
+        self.forward_backward()
         self.acc_no += 1
-        
+
+    def get_gradient(self, node):
+        """average gradient"""
+        assert node in self.acc_gradient
+        return self.acc_gradient[node] / self.acc_no
+
+    def apply_gradients(self, node_gradients_dict, summarize=False, acc_no=None):
+
+        for node, gradient in node_gradients_dict.item():
+            if isinstance(node, Node):
+                pass
+
+            else:
+                target_node = get_node_from_graph(node)
+                assert target_node is not None
+                assert self.acc_gradient[target_node].shape == gradient.shape
+                if summarize:
+                    self.acc_gradient[target_node] += gradient
+                else:
+                    self.acc_gradient[target_node] = gradient
+
+        if summarize:
+            self.acc_no += acc_no
+        else:
+            if acc_no is None:
+                self.acc_no = 1
+            else:
+                self.acc_no = acc_no
+
+    def forward_backward(self):
+
+        self.graph.clear_jacobi()
+        self.target.forward()
+
+        for node in self.graph.nodes:
+            if isinstance(node, Variable) and node.trainable:
+                node.backward(self.target)
+
+                gradient = node.jacobiMatrix.T.reshape(node.shape())
+                if node not in self.acc_gradient:
+                    self.acc_gradient[node] = gradient
+                else:
+                    self.acc_gradient[node] += gradient
+
+    @abc.abstractmethod
+    def _update(self):
+        """abstract function"""
+
+    def updata(self, var_gradients=None):
+        if var_gradients is not None:
+            self.apply_gradients(var_gradients)
+
